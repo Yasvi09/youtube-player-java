d627c467d909ff29863deb175d60f7daef0a68c0
Yasvi Vaghasiya <yasvivaghasiya119@gmail.com>
2025-02-25 17:29:14 +0530

    first commit

---
+++ app/src/main/java/com/example/youtube_java/ui/theme/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.youtube_java.ui.theme;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.youtube_java.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
